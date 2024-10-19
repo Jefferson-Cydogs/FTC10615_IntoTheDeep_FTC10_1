6d019afc7c55a27c99aea70840dac192b06784bf
R20 <cydogs.github2@gmail.com>
2024-10-18 20:05:54 -0400

    SpecimenGripperCODEv1.0                                       Elliot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/TeleOp3.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.intothedeep;
+
+
+import android.icu.text.Transliterator;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class TeleOp3 extends LinearOpMode {
+
+    // declare variables here
+    public Servo SpecimenGripper;
+
+    public double servoposition =0;
+    @Override
+    public void runOpMode() {
+
+        //    Do initialization things here
+
+        SpecimenGripper=hardwareMap.get(Servo.class,"SpecimenGripper");
+waitForStart();
+
+        while (opModeIsActive()) {
+            // do op mode things here
+
+if(gamepad2.right_bumper){
+    servoposition +=0.8;
+}
+if(gamepad2.left_bumper){
+               servoposition +=0;
+           }
+    }
+
+
+    {
+        if(gamepad1.triangle)
+        {
+            // do something if triangle is pushed
+        }
+        if(gamepad1.square)
+        {
+            // do something if square is pushed
+        }
+
+    }
+}}
